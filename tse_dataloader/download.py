a9aab1ba9238c5e9f33bb208ae022688813b2ed7
aliik7 <62479165+aliik7@users.noreply.github.com>
2020-07-12 12:44:47 +0430

    Update download.py

--- tse_dataloader/download.py
+++ tse_dataloader/download.py
@@ -41,3 +41,28 @@ def get_data(ticker):
 
     except IndexError:
         return 'No match'
+
+
+def getcode_data(code):
+    try:
+        # create url for given ticker code and sending a request for data
+        url = "http://www.tsetmc.com/tsev2/data/Export-txt.aspx?t=i&a=1&b=0&i={}"
+        response = requests.get(url.format(code))
+        file_object = io.StringIO(response.content.decode('utf-8'))
+        # read downloaded data with pandas
+        df = pd.read_csv(file_object)
+
+        # changing columns' names and date formant from int to date
+        df.columns = ['TICKER', 'DTYYYYMMDD', 'FIRST', 'HIGH', 'LOW', 'CLOSE',
+                      'VALUE', 'VOL', 'OPENINT', 'PER', 'OPEN', 'LAST']
+        df['Date'] = pd.to_datetime(df['DTYYYYMMDD'].astype(str), format='%Y%m%d')
+        df["date_shamsi"] = df["DTYYYYMMDD"].apply(convert_to_shamsi)
+        df.set_index('Date', inplace=True)
+        df.drop(columns=["DTYYYYMMDD"], inplace=True)
+        df.sort_index(inplace=True)
+
+        return df
+
+    except IndexError:
+        return 'No file found!'
+
